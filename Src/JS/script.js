395c489232b95a97c3d9e55d80afe952aedf1b32
Sona <sonvard22.khachatryan@gmail.com>
2020-02-20 22:36:53 +0300

    add 2 sections

---
+++ Src/JS/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        draggable: false,
+      });
+});
