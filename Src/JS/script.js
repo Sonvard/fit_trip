e555e914a15addae1965d694b25868decf144d5c
Sona <sonvard22.khachatryan@gmail.com>
2020-03-17 21:08:59 +0300

    add about trainers

--- Src/JS/script.js
+++ Src/JS/script.js
@@ -11,3 +11,11 @@ $(document).ready(function(){
         draggable: false,
       });
 });
+
+//team cards
+
+let firstTrainer = document.querySelector('#puchkov');
+firstTrainer.onclick = function() {
+  let cardBack1 = document.querySelector('#card-back1');
+  cardBack1.style.display = block;
+}
